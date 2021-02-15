7388b090ba7c7eabacd9e9bf5dd456eedf7c1e90
Akul Vohra <vohra.akul@gmail.com>
2021-02-14 16:24:44 -0800

    Got results to show and added sample for debug mode
    Next step is to do a .gitignore and edit design. Change to dropdown form.

--- nlr_webapp/routes.py
+++ nlr_webapp/routes.py
@@ -1,5 +1,5 @@
-from flask import url_for, redirect, render_template
-from nlr_webapp.recommender import Infer
+from flask import url_for, redirect, render_template, request
+#from nlr_webapp.recommender import Infer
 
 from nlr_webapp import app
 from nlr_webapp.forms import QueryForm
@@ -7,9 +7,17 @@ from nlr_webapp.forms import QueryForm
 @app.route("/", methods=['GET', 'POST'])
 def home():
     form = QueryForm()
+    if request.method == 'GET':
+        return render_template('home.html', form=form)
 
-    if form.validate_on_submit():
-        print("form validated")
-        title, abstractR, link = Infer(form.query.data, form.top_k.data)
-        print(title, link)
-    return render_template('home.html', form=form)
+    elif request.method == 'POST':
+        if form.validate_on_submit():
+            print("form validated")
+            #title, abstract, link = Infer(form.query.data, form.top_k.data)
+            #put flash for generating here
+            title, abstract, link = ["T1", "T2", "T3"], ["lipsum 1", "lipsum 1", "lipsum 1"], ["https://link.com", "https://link.com", "https://link.com"]
+            results = zip(title, abstract, link)
+            print("inference generated")
+            return render_template('home.html', form=form, results=results)
+        else:
+            return render_template('home.html', form=form)
